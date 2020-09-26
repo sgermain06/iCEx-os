4b2b2d85299b32131ccc5f8350ce909bc384b029
simon-germain <simon@pps.io>
2020-09-26 03:20:39 -0400

    Moved va_list to types.h

--- types.h
+++ types.h
@@ -1,6 +1,24 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
+/* va list parameter list */
+typedef uint8_t* va_list;
+/* width of stack == width of int */
+#define	STACKITEM int32_t
+ 
+/* round up width of objects pushed on stack. The expression before the
+& ensures that we get 0 for objects of size 0. */
+#define	VA_SIZE(TYPE) ((sizeof(TYPE) + sizeof(STACKITEM) - 1) & ~(sizeof(STACKITEM) - 1))
+
+/* &(LASTARG) points to the LEFTMOST argument of the function call
+(before the ...) */
+#define	va_start(AP, LASTARG) (AP=((va_list)&(LASTARG) + VA_SIZE(LASTARG)))
+
+/* nothing for va_end */
+#define va_end(AP)
+
+#define va_arg(AP, TYPE) (AP += VA_SIZE(TYPE), *((TYPE *)(AP - VA_SIZE(TYPE))))
+
 typedef char int8_t;
 typedef unsigned char uint8_t;
 
